7219b392946f5728e2233a3d8ed4ff39f605729b
Aibek Minbaev <aibekminbaev050402@gmail.com>
2024-05-06 15:09:41 +0900

    done: added react routing - to do: create components for activity types

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,24 +1,31 @@
 import './Header.css';
+import { Link, Routes, Route } from 'react-router-dom'
+
+//import pages 
+import HomePage from '../../pages/HomePage';
+import ExplorePage from '../../pages/ExplorePage'
+import CollectionsPage from '../../pages/CollectionsPage'
 
 function Header() {
     return (
         <header className='header'>
             <div className='header-inner'>
-                {/* <img className='logo' src={'images/Logo.svg'} alt='' /> */}
-
                 <nav>
                     <ul>
-                        <li><a href=''>Home</a></li>
-                        <li><a href=''>Explore</a></li>
-                        <li><a href=''>Collections</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/explore">Explore</Link></li>
+                        <li><Link to="/collections">Collections</Link></li>
                     </ul>
                 </nav>
-                {/*                    
-                    <div className='login'>
-                    </div> */}
+
+                <Routes>
+                    <Route path='/*' element={<HomePage />} />
+                    <Route path="/explore" element={<ExplorePage />} />
+                    <Route path="/collections" element={<CollectionsPage />} />
+                </Routes>
             </div>
         </header>
-    )
+    );
 }
 
-export default Header; 
+export default Header;
