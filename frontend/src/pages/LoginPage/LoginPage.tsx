96ff9fbc55f0a205af4532eb6b33ff4d9d96ce5b
Aibek Minbaev <aibekminbaev050402@gmail.com>
2024-06-09 04:04:02 +0900

    done: switeched to auto deploy of front. started working on login/signup. to do: deploy backend

---
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    const handleLogin = () => {
+        // Perform login logic here
+        console.log('Logging in...');
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form>
+                <div>
+                    <label>Email:</label>
+                    <input type="email" value={email} onChange={handleEmailChange} />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input type="password" value={password} onChange={handlePasswordChange} />
+                </div>
+                <button type="button" onClick={handleLogin}>Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default LoginPage;
