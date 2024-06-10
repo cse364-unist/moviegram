67570bbd1ef5c2a31225313e13aebae47a476699
Aibek Minbaev <aibekminbaev050402@gmail.com>
2024-06-10 15:27:23 +0900

    done: migrated to postgre and deployed db. to do: prepare backend for deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os 
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -70,16 +72,36 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #     }
 # }
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'moviegram',
+#         'USER':'root',
+#         'PASSWORD':'root',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
+# local postgresql
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'moviegram',
+#         'USER': 'moviegram',
+#         'PASSWORD': 'asdf',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+# remote postgresql
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'moviegram',
-        'USER':'root',
-        'PASSWORD':'root',
-        'HOST': 'localhost',   
-        'PORT': '3306', 
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'))
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
