35747d0a6f2a478af612110eb9a1acd7b34a266c
Makida <makidagebregiorgis@gmail.com>
2024-06-16 06:20:17 +0900

    Revert "Updated CollectionsPage.txt"
    This reverts commit 5496f3c560b09d3db8f5234413b5f9a5c054f590.

--- frontend/src/pages/CollectionsPage/CollectionsPage.tsx
+++ frontend/src/pages/CollectionsPage/CollectionsPage.tsx
@@ -1,4 +1,4 @@
-/*import CollectionCard from '../../components/Collection/CollectionCard'
+import CollectionCard from '../../components/CollectionCard/CollectionCard'
 export default function CollectionsPage() {
     return (
         <div className="flex w-70 pt-16">
@@ -9,62 +9,4 @@ export default function CollectionsPage() {
             <aside className="w-20"></aside>
         </div>
     )
-}*/
-import CollectionsList from '../../components/Collection/CollectionsList';
-import PopularCollections from '../../components/Collection/PopularCollections';
-
-import darkKnightPoster from '../../../public/images/dark_knight.jpg'; // Import the image
-import inceptionPoster from '../../../public/images/inception.jpg';
-import madmaxPoster from '../../../public/images/mad_max.jpg';
-import diehardPoster from '../../../public/images/die_hard.jpg';
-
-const collectionsData = [
-  {
-    title: 'Action Movies Collection',
-    movies: [
-      { title: 'The Dark Knight', poster: darkKnightPoster },
-      { title: 'Inception', poster: inceptionPoster },
-      { title: 'Mad Max: Fury Road', poster: madmaxPoster },
-      { title: 'Die Hard', poster: diehardPoster }
-    ]
-  },
-  {
-    title: 'Comedy Movies Collection',
-    movies: [
-      { title: 'Superbad', poster: 'path/to/superbad.jpg' },
-      { title: 'The Hangover', poster: 'path/to/hangover.jpg' },
-      { title: 'Dumb and Dumber', poster: 'path/to/dumb_and_dumber.jpg' },
-      { title: 'Anchorman: The Legend of Ron Burgundy', poster: 'path/to/anchorman.jpg' }
-    ]
-  },
-  {
-    title: 'Action Movies Collection',
-    movies: [
-      { title: 'The Dark Knight', poster: darkKnightPoster },
-      { title: 'Inception', poster: inceptionPoster },
-      { title: 'Mad Max: Fury Road', poster: madmaxPoster },
-      { title: 'Die Hard', poster: diehardPoster }
-    ]
-  },
-  {
-    title: 'Action Movies Collection',
-    movies: [
-      { title: 'The Dark Knight', poster: darkKnightPoster },
-      { title: 'Inception', poster: inceptionPoster },
-      { title: 'Mad Max: Fury Road', poster: madmaxPoster },
-      { title: 'Die Hard', poster: diehardPoster }
-    ]
-  }
-];
-
-export default function CollectionsPage() {
-    return (
-        <div>
-        <main className="mt-20">
-          <CollectionsList collections={collectionsData} />
-          <div className="mt-8"></div>
-          <PopularCollections collections={collectionsData} />
-        </main>
-    </div>
-    )
-}
+} 
